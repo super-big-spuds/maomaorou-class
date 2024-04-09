a180d3265e3a0bde876cf0637d081cc19ffc4147
agry88 <rgok307085@gmail.com>
2024-04-09 21:33:36 +0800

    fix: wrong schema gen command.

--- maomaorou-class-web/codegen.ts
+++ maomaorou-class-web/codegen.ts
@@ -1,8 +1,8 @@
-import { PUBLIC_BACKEND_URL } from "@/lib/env";
+import { LOCAL_BACKEND_URL } from "./src/lib/env";
 import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: `${PUBLIC_BACKEND_URL}/graphql`,
+  schema: `${LOCAL_BACKEND_URL}/graphql`,
   // this assumes that all your source files are in a top-level `src/` directory - you might need to adjust this to your file structure
   documents: ["src/**/*.{ts,tsx}"],
   generates: {
