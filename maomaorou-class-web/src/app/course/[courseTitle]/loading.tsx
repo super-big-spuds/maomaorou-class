34eea53e4fca5610a8d6a71be69fbb3fd2d12b48
jiweihong0 <jiweihong0@gmail.com>
2024-04-19 18:41:05 +0800

    feat: skelton loading page

--- maomaorou-class-web/src/app/course/[courseTitle]/loading.tsx
+++ maomaorou-class-web/src/app/course/[courseTitle]/loading.tsx
@@ -1,32 +1,34 @@
 export default function CourseTitleLoadingPage() {
   return (
-    <div className="flex flex-wrap justify-center relative h-full animate-pulse">
-      <div className="flex flex-col px-10 py-5 gap-6 max-w-3xl">
+    <div className="flex flex-wrap justify-center relative h-full min-h-screen  bg-slate-100 ">
+      <div className="flex flex-col px-10 py-5 gap-6 max-w-3xl min-w-[25vw] border bg-white ">
         <p className="text-3xl font-bold"></p>
-        <p className=" text-2xl font-bold">關於此課程</p>
-
-        <p className=" animate-bounce">加載中 ...</p>
-        <p className=" text-2xl font-bold ">你將會學到什麼?</p>
-        <p className=" animate-bounce">加載中 ...</p>
-        <p className=" text-2xl font-bold">課程大綱</p>
-        <p className=" animate-bounce">加載中 ...</p>
+        <p className=" w-[150px] h-[35px] bg-slate-300 rounded-full animate-pulse"></p>
+        <p className=" w-[400px] h-[300px] bg-slate-300 rounded-2xl animate-pulse"></p>
+        <p className=" w-[200px] h-[20px] bg-slate-300 rounded-full animate-pulse"></p>
+        <p className=" w-[300px] h-[20px] bg-slate-300 rounded-full animate-pulse"></p>
+        <p className=" w-[200px] h-[20px] bg-slate-300 rounded-full animate-pulse"></p>
+        <p className=" w-[300px] h-[20px] bg-slate-300 rounded-full animate-pulse"></p>
+        <p className=" w-[200px] h-[20px] bg-slate-300 rounded-full animate-pulse"></p>
+        <p className=" w-[400px] h-[60px] bg-slate-300 rounded-2xl animate-pulse"></p>
+        <p className=" w-[400px] h-[60px] bg-slate-300 rounded-2xl animate-pulse"></p>
+        <p className=" w-[400px] h-[60px] bg-slate-300 rounded-2xl animate-pulse"></p>
       </div>
 
-      <div className="sticky top-20 flex flex-col  mt-5 w-full  lg:w-1/6 sm:w-1/2 h-full bg-neutral-200 p-3 items-center rounded shadow-xl">
+      <div className="sticky top-20 flex flex-col  mt-5 w-full  lg:w-1/6 sm:w-1/2 h-full bg-neutral-200 p-3 items-center rounded shadow-xl ">
         <div className=" bg-white rounded border p-2 w-4/5 lg:w-full flex flex-col gap-3 ">
-          <p className=" text-xl font-bold">課程價格</p>
-          <p className=" animate-bounce">加載中 ...</p>
-          <p className=" text-2xl ml-3"></p>
+          <p className=" w-[80px] h-[20px] bg-slate-300 rounded-full animate-pulse"></p>
+          <p className=" w-[200px] h-[20px] bg-slate-300 rounded-full animate-pulse"></p>
 
-          <p className=" text-gray-400 font-xs">最後更新{}</p>
-          <p className=" animate-bounce">加載中 ...</p>
+          <p className=" w-[200px] h-[20px] bg-slate-300 rounded-full animate-pulse"></p>
+          <p className=" w-[80px] h-[20px] bg-slate-300 rounded-full animate-pulse"></p>
         </div>
         <div className=" bg-slate-100 rounded border p-2 w-4/5 lg:w-full flex flex-col gap-3 ">
-          <p className=" text-xl font-bold">課程包含</p>
-          <p className=" animate-bounce">加載中 ...</p>
+          <p className="w-[60px] h-[20px] bg-slate-300 rounded-full animate-pulse"></p>
+          <p className=" w-[100px] h-[20px] bg-slate-300 rounded-full animate-pulse"></p>
 
-          <p className=" text-xl font-bold">適合對象</p>
-          <p className=" animate-bounce">加載中 ...</p>
+          <p className="w-[60px] h-[20px] bg-slate-300 rounded-full animate-pulse"></p>
+          <p className=" w-[100px] h-[20px] bg-slate-300 rounded-full animate-pulse"></p>
         </div>
       </div>
     </div>
--- maomaorou-class-web/src/app/course/[courseTitle]/page.tsx
+++ maomaorou-class-web/src/app/course/[courseTitle]/page.tsx
@@ -133,8 +133,8 @@ export default async function CoursePage({
   // price
 
   return (
-    <div className="flex flex-wrap justify-center relative h-full">
-      <div className="flex flex-col px-10 py-5 gap-6 max-w-3xl">
+    <div className="flex flex-wrap justify-center relative h-full min-h-screen  bg-slate-100">
+      <div className="flex flex-col px-10 py-5 gap-6 max-w-3xl min-w-[25vw] border bg-white ">
         <p className="text-3xl font-bold">
           線上影音課程-{data.courseByTitle.data.attributes.title}
         </p>
@@ -182,7 +182,7 @@ export default async function CoursePage({
       </div>
 
       <div className="sticky top-20 flex flex-col  mt-5 w-full  lg:w-1/6 sm:w-1/2 h-full bg-neutral-200 p-3 items-center rounded shadow-xl">
-        <div className=" bg-white rounded border p-2 w-4/5 lg:w-full flex flex-col gap-3 ">
+        <div className=" bg-white rounded border p-2 w-4/5 lg:w-full flex flex-col gap-3  ">
           <p className=" text-xl font-bold">課程價格</p>
           <p className=" text-2xl ml-3">
             NT{data.courseByTitle.data.attributes.price.toLocaleString()}
@@ -194,6 +194,7 @@ export default async function CoursePage({
           />
           <p className=" text-gray-400 font-xs">最後更新{}</p>
         </div>
+
         <div className=" bg-slate-100 rounded border p-2 w-4/5 lg:w-full flex flex-col gap-3 ">
           <p className=" text-xl font-bold">課程包含</p>
           <ul className="ml-3">
