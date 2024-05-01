af678afd2c2c312196f9126525a236385f3f040d
agry88 <rgok307085@gmail.com>
2024-05-01 16:09:53 +0800

    feat: make video element load faster.

--- maomaorou-class-web/src/components/ui/blocked-download-video.tsx
+++ maomaorou-class-web/src/components/ui/blocked-download-video.tsx
@@ -5,13 +5,33 @@ type Props = {
 };
 
 export default function BlockedDownloadVideo({ url }: Props) {
+  const regex = /.*\.s3(.*)\.amazonaws\.com/;
+  const newGroupValue = "-accelerate";
+
+  const match = regex.exec(url);
+
+  function getAwsReplacedUrl() {
+    if (match) {
+      const replacedText = url.replace(regex, function (match, group1) {
+        return `${match.slice(
+          0,
+          match.indexOf(group1)
+        )}${newGroupValue}${match.slice(match.indexOf(group1) + group1.length)}`;
+      });
+      return replacedText;
+    }
+
+    return url;
+  }
+
   return (
     <video
       controls
       controlsList="nodownload"
+      preload="metadata"
       onContextMenu={(e) => e.preventDefault()}
     >
-      <source src={url} type="video/mp4" />
+      <source src={getAwsReplacedUrl()} type="video/mp4" />
       您的瀏覽器不支援線上撥放影片
     </video>
   );
